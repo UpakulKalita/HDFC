18a2560d2e12477d2df250bf28c52b0390378db4
ijaj07 <iamijaj007@gmail.com>
2026-01-20 11:23:01 +0530

    Add files via upload

---
+++ lib/dashboard/presentation/widgets/content/promo_banner.dart
@@ -0,0 +1,79 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+class PromoBanner extends StatefulWidget {
+  const PromoBanner({super.key});
+
+  @override
+  State<PromoBanner> createState() => _PromoBannerState();
+}
+
+class _PromoBannerState extends State<PromoBanner> {
+  final PageController _controller = PageController();
+  int _currentIndex = 0;
+
+  final List<String> banners = [
+    "assets/banners/1.png",
+    "assets/banners/2.png",
+    "assets/banners/3.png",
+    "assets/banners/4.png",
+  ];
+
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _startAutoSlide();
+  }
+
+  void _startAutoSlide() {
+    _timer = Timer.periodic(const Duration(seconds: 3), (timer) {
+      if (!_controller.hasClients) return;
+
+      _currentIndex =
+          (_currentIndex + 1) % banners.length;
+
+      _controller.animateToPage(
+        _currentIndex,
+        duration: const Duration(milliseconds: 800),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 10 / 3, // professional dashboard ratio
+      child: PageView.builder(
+        controller: _controller,
+        itemCount: banners.length,
+        itemBuilder: (context, index) {
+          return _buildBanner(banners[index]);
+        },
+      ),
+    ).animate().fadeIn().moveY(begin: 10, end: 0);
+  }
+
+  Widget _buildBanner(String imagePath) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Image.asset(
+          imagePath,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
